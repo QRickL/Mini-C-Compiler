6e75cf8d2f6213cd4cb19b8e3e0ee9881169a807
Rick Li <rickli1738@gmail.com>
2025-11-01 23:21:09 -0400

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "WLP4 Compiler: by" << std::endl;
+
+  std::cout << "Rick Li" << std::endl;
+
+  std::cerr << "Code not publicly available due to the University of Waterloo's Policy 71 regulations" << std::endl;
+  return 1;
+  
+}
